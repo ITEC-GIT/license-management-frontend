ddaa0da4aa1d73ffdcdddabec0cd816c56ead6ce
zoughaib-sally <s.zoughaib@itec.co.ao>
2026-05-22 09:39:04 +0300

    Update Vite configuration to change server port from 3000 to 5556 for improved development environment setup.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5556,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://cobalt.pulsar.ao:5557',
